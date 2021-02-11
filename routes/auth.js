011797a3a2a375d95bee4ca76563943a55369490
amontalvof <amontalvof92@gmail.com>
2021-02-11 13:42:09 -0600

    feat: endpoints renew, register and login

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,12 @@
 
 const { Router } = require('express');
 const router = Router();
+const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
-router.get('/', (req, res) => {
-    res.json({
-        ok: true,
-    });
-});
+router.post('/new', createUser);
+
+router.post('/', loginUser);
+
+router.get('/renew', renewToken);
 
 module.exports = router;
