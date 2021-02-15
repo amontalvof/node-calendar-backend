8b37274c3ea0163886e771fcb5d05e9dbfe1d6f6
amontalvof <amontalvof92@gmail.com>
2021-02-14 18:29:58 -0600

    feat: encrypt password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const createUser = async (req, res = response) => {
@@ -14,6 +15,11 @@ const createUser = async (req, res = response) => {
         }
 
         user = new User(req.body);
+
+        // encrypt password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+
         await user.save();
 
         res.status(201).json({
