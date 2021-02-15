790b9a4e2c11bbbcbcd37e259cd1d0b54ef73ab5
amontalvof <amontalvof92@gmail.com>
2021-02-14 18:14:33 -0600

    feat: user validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,33 @@
 const { response } = require('express');
+const User = require('../models/User');
 
-const createUser = (req, res = response) => {
-    const { name, email, password } = req.body;
+const createUser = async (req, res = response) => {
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
 
-    res.status(201).json({
-        ok: true,
-        message: 'register',
-        name,
-        email,
-        password,
-    });
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                message: 'There is already a user with that email',
+            });
+        }
+
+        user = new User(req.body);
+        await user.save();
+
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            message: 'Error, please talk to the administrator.',
+        });
+    }
 };
 
 const loginUser = (req, res = response) => {
